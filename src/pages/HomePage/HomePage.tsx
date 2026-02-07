a50b3e9211fde3f629fd69ea5173a52478cf3848
ianmoore228 <on1920030320587@yandex.ru>
2026-02-07 15:48:36 +0200

    chore: init

---
+++ src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,10 @@
+import type { FC } from "react";
+import styles from "@/pages/HomePage/HomePage.module.css";
+
+export const HomePage: FC = () => {
+    return (
+    <div className={styles.homePage}>
+        <h1>yaml parser</h1>
+    </div>
+    )
+}
