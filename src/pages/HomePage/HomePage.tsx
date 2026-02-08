931b584c7e0cdda611bdb1fe1e4ea5bd1ebd1e05
ianmoore228 <on1920030320587@yandex.ru>
2026-02-08 21:26:36 +0200

    feat: update home page

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,10 +1,15 @@
 import type { FC } from "react";
 import styles from "@/pages/HomePage/HomePage.module.css";
+import { YamlSubmit } from "@/widgets/YamlSubmit";
+import { YamlStructure } from "@/widgets/YamlStructure";
 
 export const HomePage: FC = () => {
     return (
     <div className={styles.homePage}>
-        <h1>yaml parser</h1>
+        <h1 className={styles.homePageTitle}>.yaml parser</h1>
+        <YamlSubmit/>
+        <div className={styles.line}></div>
+        <YamlStructure/>
     </div>
     )
 }
