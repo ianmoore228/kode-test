3c7a5ef4e30406dabcf44ecec046fe2baa754b0e
ianmoore228 <on1920030320587@yandex.ru>
2026-02-08 21:19:41 +0200

    feat: update slice

--- src/entities/openApi/index.ts
+++ src/entities/openApi/index.ts
@@ -1,4 +1,3 @@
 export { type OpenApiProps} from  "./model/openApi"
 export { type ObjectProps} from  "./model/openApi"
-export { openApiReducer } from "./slice/OpenApiSlice"
-export { setOpenApiData } from "./slice/OpenApiSlice"
+export { openApiReducer, selectOpenApi, setOpenApiData, selectFile } from "./slice/OpenApiSlice"
--- src/entities/openApi/slice/OpenApiSlice.tsx
+++ src/entities/openApi/slice/OpenApiSlice.tsx
@@ -4,15 +4,19 @@ import { type ObjectProps } from "@/entities/openApi";
 type OpenApiState = {
   components: ObjectProps | null;
   paths: ObjectProps | null;
+  name: string;
   other: {
     [key: string]: ObjectProps;
   };
+  selectedFile: string | null;
 };
 
 const initialState: OpenApiState = {
   components: null,
   paths: null,
+  name: "",
   other: {},
+  selectedFile: null,
 };
 
 const openApiSlice = createSlice({
@@ -24,17 +28,31 @@ const openApiSlice = createSlice({
       action: PayloadAction<{
         components: ObjectProps;
         paths: ObjectProps;
+        name: string;
         other: {
           [key: string]: ObjectProps;
         };
+        selectedFile: string | null;
       }>
     ) {
       state.components = action.payload.components;
       state.paths = action.payload.paths;
+      state.name = action.payload.name;
       state.other = action.payload.other;
+      state.selectedFile = action.payload.selectedFile;
+      console.log(state.selectedFile);
+    },
+    selectFile(state, action: PayloadAction<string>) {
+      state.selectedFile = action.payload;
+      console.log(state.selectedFile);
     },
   },
 });
 
+export const selectOpenApi = (state: { openApi: OpenApiState }) =>
+  state.openApi;
+
 export const { setOpenApiData } = openApiSlice.actions;
 export const openApiReducer = openApiSlice.reducer;
+
+export const { selectFile } = openApiSlice.actions;
