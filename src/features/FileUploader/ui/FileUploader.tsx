dae5dfea4697a6d5621f90c2c0aff22434b903ef
ianmoore228 <on1920030320587@yandex.ru>
2026-02-08 15:09:13 +0200

    feat: updated file & text uploader

--- src/features/FileUploader/ui/FileUploader.tsx
+++ src/features/FileUploader/ui/FileUploader.tsx
@@ -1,28 +1,50 @@
 import type { FC } from "react";
 import styles from "./FileUploader.module.css"
+import { useDispatch } from "react-redux";
+import { parseYaml } from "@/features/ParseYaml";
+import { setOpenApiData } from "@/entities/openApi";
 
 export const FileUploader: FC = () => {
+
+  const dispatch = useDispatch();
+
 
 function handleFileUpload(e: React.ChangeEvent<HTMLInputElement>) {
   const files = e.target.files;
   if (!files) return;
-
   const file = files[0];
-
   const fileExtension = file.name.split(".")[1];
 
-  console.log(fileExtension)
   if (fileExtension !== "yaml") {
    console.log("error")
+   return
   }
 
   const reader = new FileReader();
 
   reader.readAsText(file)
   reader.onload = () => {
-    console.log(reader.result)
+    if (typeof reader.result !== 'string') return;
+
+    try {
+      const parsed = parseYaml(reader.result);
+
+      const { components, paths, ...other } = parsed;
+
+      dispatch(
+        setOpenApiData({
+          components,
+          paths,
+          other
+        })
+      );
+
+      console.log(components, paths, other)
+    } catch (e) {
+      console.error(e);
+    }
+  };
   }
-}
 
   return (
   <div className={styles.fileUploader}>
@@ -37,3 +59,4 @@ function handleFileUpload(e: React.ChangeEvent<HTMLInputElement>) {
   </div>
   );
 };
+
--- src/features/TextUploader/ui/TextUploader.tsx
+++ src/features/TextUploader/ui/TextUploader.tsx
@@ -1,11 +1,42 @@
 import type { FC } from "react";
 import styles from "./TextUploader.module.css"
+import { useDispatch } from "react-redux";
+import { parseYaml } from "@/features/ParseYaml";
+import { setOpenApiData } from "@/entities/openApi";
 
 export const TextUploader: FC = () => {
+
+    const dispatch = useDispatch();
+
+    function handleSubmit() {
+        console.log("submit")
+        const yamlText = document.getElementById("yamlText") as HTMLInputElement;
+      try {
+          const parsed = parseYaml(yamlText.value);
+    
+          const { components, paths, ...other } = parsed;
+    
+          dispatch(
+            setOpenApiData({
+              components,
+              paths,
+              other
+            })
+          );
+    
+          console.log(components, paths, other)
+        } catch (e) {
+          console.error(e);
+        }
+      
+    }
+
     return (
         <div className={styles.textUploader}>
-        <textarea id="yamlText" className={styles.textUploaderInput} placeholder="Вставтьте текст из файла .yaml"></textarea>
-        <button className={styles.textUploaderButton} type="submit">Парсинг</button>
+        <textarea id="yamlText" className={styles.textUploaderInput} placeholder="Вставтьте текст спецификации openapi.yaml"></textarea>
+        <button
+        onClick={handleSubmit}
+        className={styles.textUploaderButton} type="submit">Парсинг</button>
         </div>
     )
 }
