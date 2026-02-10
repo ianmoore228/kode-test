07250cc2b12c49efe1f05f853057b0f1a10c8f3c
ianmoore228 <on1920030320587@yandex.ru>
2026-02-10 22:06:37 +0200

    fix: update openapi type

--- src/entities/openApi/model/openApi.ts
+++ src/entities/openApi/model/openApi.ts
@@ -1,14 +1,14 @@
-export type ObjectProps = {
-    [key: string]: string | ObjectProps;
-  };
-  
-  type OtherOpenApiProps = {
-    [key: string]: string | ObjectProps;
-  };
-  
-  export type OpenApiProps = OtherOpenApiProps & {
-    openapi: string;
-    components: ObjectProps;
-    paths: ObjectProps;
-  };
-  
+export type ObjectProps =
+  | string
+  | number
+  | boolean
+  | null
+  | ObjectProps[]
+  | { [key: string]: ObjectProps };
+
+export interface OpenApiProps {
+  openapi: string;
+  paths: Record<string, ObjectProps>;
+  components: Record<string, ObjectProps>;
+  [key: string]: ObjectProps;
+}
